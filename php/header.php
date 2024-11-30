5270b90b498278eb9a7fb1759d993364a279b651
Wesley-Baker <wgb10097@uga.edu>
2024-11-29 20:33:13 -0500

    Initial creation of header.php

---
+++ php/header.php
@@ -0,0 +1,24 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon Info</title>
+    <link rel="stylesheet" href="css\styles.css"> <!-- Linking CSS -->
+</head>
+<body>
+    <header class="site-header">
+        <!-- Banner Image -->
+        <div class="banner">
+            <img src="C:\Users\wesle\Desktop\Coding Stuff\NationalPokedexRepo\NationalPokedex\img\image.png" alt="Pokemon Banner" class="banner-image">
+        </div>
+        <!-- Navigation Bar -->
+        <nav class="navbar">
+            <ul class="nav-list">
+                <li><a href="nationalpokedex.php" class="nav-link">National Pokedex</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
